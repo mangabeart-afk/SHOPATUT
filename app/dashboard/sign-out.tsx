f973925539691e61678e8169dd9a19db392baeea
mangabeart-afk <mangabeart@gmail.com>
2026-09-01 22:52:04 +0200

    Delete app directory

--- app/dashboard/sign-out.tsx
+++
@@ -1,3 +0,0 @@
-'use client'
-import { createClient } from '../../lib/supabase-browser'
-export default function SignOutButton(){ return <button className="signout" onClick={async()=>{await createClient().auth.signOut(); window.location.assign('/login')}}>Esci</button> }
